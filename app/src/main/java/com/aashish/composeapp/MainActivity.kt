502b334ceb8f62911ebd16e6e27d574aa7440beb
aashish-ranjan <aashishranjan2011@gmail.com>
2022-08-02 01:39:33 +0530

    row, col & basic sizing

--- app/src/main/java/com/aashish/composeapp/MainActivity.kt
+++ app/src/main/java/com/aashish/composeapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.aashish.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aashish.composeapp.ui.theme.ComposeAppTheme
@@ -17,21 +19,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                Greeting("Aashish")
+                HorizontallyEndVerticallyCenteredSingleText(text = "hEndVCenter")
+                HorizonatallyEquiSeparatedVerticallyBottomAlignedTexts()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HorizontallyEndVerticallyCenteredSingleText(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun HorizonatallyEquiSeparatedVerticallyBottomAlignedTexts() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom
+
+    ) {
+        Text("start")
+        Text("center")
+        Text("end")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Greeting("Ranjan")
+
     }
 }
