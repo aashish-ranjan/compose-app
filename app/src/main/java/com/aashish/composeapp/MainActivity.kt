054c11779a7b937fdb3baf90a1c0005938099cbd
aashish-ranjan <aashishranjan2011@gmail.com>
2022-08-02 03:16:45 +0530

    modifiers borders, padding, spacer and offset

--- app/src/main/java/com/aashish/composeapp/MainActivity.kt
+++ app/src/main/java/com/aashish/composeapp/MainActivity.kt
@@ -3,15 +3,15 @@ package com.aashish.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aashish.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                HorizontallyEndVerticallyCenteredSingleText(text = "hEndVCenter")
-                HorizonatallyEquiSeparatedVerticallyBottomAlignedTexts()
+                TriNestedBlockWithOffsetOnInnerBlockHavingText()
             }
         }
     }
@@ -50,10 +49,36 @@ fun HorizonatallyEquiSeparatedVerticallyBottomAlignedTexts() {
     }
 }
 
+@Composable
+fun TriNestedBlockWithOffsetOnInnerBlockHavingText() {
+    Column(
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth(0.9f)
+            .border(5.dp, Color.Green)
+            .padding(5.dp)
+            .border(10.dp, Color.Yellow)
+            .padding(10.dp)
+            .border(20.dp, Color.Red)
+            .padding(20.dp)
+
+    ) {
+        Text(text = "Ranjan")
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = "Aashish",
+            modifier = Modifier
+                .offset(10.dp, 20.dp)
+                .border(2.dp, Color.Black)
+                .padding(2.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-
+        TriNestedBlockWithOffsetOnInnerBlockHavingText()
     }
 }
